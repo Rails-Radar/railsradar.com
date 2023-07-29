e1f9fe05cb3112c29c4045fab6c963bc1033e71f
Maksim Vakarchuk <11082252+maks112v@users.noreply.github.com>
2023-07-29 16:01:24 -0400

    working on auth

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,14 @@ Rails.application.routes.draw do
   resources :interesting_things
   resources :team_users
   resources :teams
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'omniauth_callbacks',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'page#index'
 end
