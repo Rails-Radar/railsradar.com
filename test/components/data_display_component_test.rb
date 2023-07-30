7c92b66394c492b0f14369a534e6e4a272e36082
Paul Lemus <paullemus@protonmail.com>
2023-07-29 23:57:09 -0700

    feat: added data display component

---
+++ test/components/data_display_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class DataDisplayComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(DataDisplayComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
