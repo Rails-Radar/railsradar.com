cf73257163072a92bb62a81d37a42aa065aca9b3
Maksim Vakarchuk <11082252+maks112v@users.noreply.github.com>
2023-07-30 17:56:00 -0400

    fixed tabs

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -1,7 +1,13 @@
 class CommunityController < ApplicationController
-  def index
-  end
+  before_action :set_team
 
-  def show
+  def index; end
+
+  def show; end
+
+  private
+
+  def set_team
+    @team = Team.where(is_community: true).first
   end
 end
