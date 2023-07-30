fd9798558460c26c43a2a06c86cd8f15647beb19
Paul Lemus <paullemus@protonmail.com>
2023-07-30 01:41:43 -0700

    refactor: updated button for link/btn/arrows
    Buttons now have the following options: theme, variants, link, arrows.

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -1,31 +1,77 @@
-# frozen_string_literal: true
-
+# app/components/button_component.rb
 class ButtonComponent < ViewComponent::Base
-  VARIANTS = %w[default secondary].freeze
-
-  def initialize(text:, variant: 'default')
-    @variant = variant
+  def initialize(text:, variant: 'default', theme: 'light', arrow: false, link: nil)
     @text = text
+    @variant = variant.to_sym
+    @theme = theme.to_sym
+    @arrow = arrow
+    @link = link
   end
 
   def call
-    tag.button(button_content, class: button_classes, type: 'button')
+    if @link
+      link_to_button
+    else
+      regular_button
+    end
   end
 
   private
 
+  def link_to_button
+    link_to(@link, class: link_classes) do
+      button_content
+    end
+  end
+
+  def regular_button
+    button_tag(class: button_classes, type: 'button') do
+      button_content
+    end
+  end
+
   def button_content
-    @text
+    if @arrow
+      "#{@text} #{arrow_icon}".html_safe
+    else
+      @text
+    end
   end
 
   def button_classes
+    classes = 'inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold'
+    classes += " #{theme_class}" + " #{variant_class}" # Add theme and variant classes
+    classes
+  end
+
+  def link_classes
+    classes = 'underline'
+    classes += " #{theme_class}" + " #{variant_class}" # Add theme and variant classes
+    classes += ' inline-flex items-center' if @arrow
+    classes
+  end
+
+  def theme_class
+    @theme == :dark ? 'text-white' : 'text-gray-900'
+  end
+
+  def variant_class
     case @variant
-    when 'default'
-      'inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
-    when 'secondary'
-      'ml-3 inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 bg-indigo-40'
+    when :default
+      'bg-white hover:bg-gray-50 focus:bg-gray-100'
+    when :secondary
+      @theme == :dark ? 'bg-indigo-700 hover:bg-indigo-800 focus:bg-indigo-800' : 'bg-indigo-600 hover:bg-indigo-700 focus:bg-indigo-700'
+    when :link
+      @theme == :dark ? 'text-indigo-400 hover:text-indigo-500' : 'text-blue-500 hover:text-blue-700'
+    when :link_with_arrow
+      @theme == :dark ? 'text-indigo-400 hover:text-indigo-500' : 'text-blue-500 hover:text-blue-700'
     else
       raise ArgumentError, "Invalid button variant: #{@variant}"
     end
+  end
+
+  def arrow_icon
+    # I was going to use an SVG but this works better
+    '->'
   end
 end
